69e5df619ed816ad7af54afbe98f6589b4ea26c6
Pavel Belikov <P.I.Belikov@gmail.com>
2023-03-09 12:16:22 +0300

    add test against __FILE__ macro

--- tests/client_test.go
+++ tests/client_test.go
@@ -30,6 +30,51 @@ func Test_compileMainCpp(t *testing.T) {
 	}
 }
 
+func Test_relativePath(t *testing.T) {
+	var cmdLineStr = "g++ -c dt/path.cpp -o dt/path.cpp.o -std=gnu++17"
+	exitCode, stdout, stderr, err := createClientAndEmulateDaemonForTesting(cmdLineStr)
+	if err != nil {
+		t.Errorf("Error initing nocc client %s", err)
+		return
+	}
+
+	if exitCode != 0 {
+		t.Errorf("exitCode %d\nstdout %s\nstderr %s", exitCode, stdout, stderr)
+		return
+	}
+
+	if len(stdout) > 0 {
+		t.Logf("stdout %s\n", stdout)
+	}
+	if len(stderr) > 0 {
+		t.Logf("stderr %s\n", stderr)
+	}
+
+	exitCode, stdout, err = runCmdLocallyForTesting("g++ dt/path.cpp.o -o dt/path -std=gnu++17")
+	if err != nil || exitCode != 0 {
+		t.Errorf("exitCode %d\n%s", exitCode, stdout)
+		return
+	}
+
+	if len(stdout) > 0 {
+		t.Logf("stdout %s\n", stdout)
+	}
+
+	exitCode, stdout, err = runCmdLocallyForTesting("./dt/path")
+	if err != nil || exitCode != 0 {
+		t.Errorf("exitCode %d\n%s", exitCode, stdout)
+		return
+	}
+
+	if len(stdout) > 0 {
+		t.Logf("stdout %s\n", stdout)
+	}
+
+	if string(stdout) != "dt/path.cpp\n" {
+		t.Errorf("%s", stdout)
+	}
+}
+
 func Test_compileCMakeProject1(t *testing.T) {
 	if err := runDaemonInBackgroundForTesting(); err != nil {
 		t.Error(err)
